d6f1c55db55b43eeb38683c8c4844dc652ab7a7b
Tarcísio Eduardo Moreira Crocomo <tcrocomo@esss.co>
2020-03-02 18:28:40 -0300

    Small refactor to init.sh.

--- init.sh
+++ init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+install-scripts() {
+    local workspace = "${HOME}/workspace"
+    local scripts="${workspace}/scripts"
+
+    mkdir -p "${workspace}" &&
+    [[ ! -d "${scripts}" ]] &&
+        git clone git@gitlab.com:tarcisioe/scripts "${scripts}"
+}
+
 git submodule init
 git submodule update
 bash link.sh
@@ -8,7 +17,4 @@ bash migrate.sh
 curl -fLo files/vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-
-mkdir -p ${HOME}/workspace
-scripts="${HOME}/workspace/scripts"
-[[ -d "${scripts}" ]] || git clone git@gitlab.com:tarcisioe/scripts "${scripts}"
+install-scripts
