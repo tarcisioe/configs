429fbc0f68569f00a79de48297066d867273195e
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2021-03-09 17:51:07 -0300

    Avoid removing pyenv bin and shims.

--- files/shutils/pyenv.sh
+++ files/shutils/pyenv.sh
@@ -6,6 +6,4 @@ if not-root
 then
     add-path "${HOME}/.local/share/pyenv/bin"
     eval "$(pyenv init -)"
-    remove-path "${HOME}/.local/share/pyenv/bin"
-    remove-path "${HOME}/.local/share/pyenv/shims"
 fi
