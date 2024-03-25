db4bfa5184d5a9a29c0c618cef93e19d68e7d2ce
Tarcísio Eduardo Moreira Crocomo <tarcisioe@pm.me>
2024-03-25 12:17:50 -0300

    Use rocks-config.nvim as well.

--- files/nvim/lua/plugins.lua
+++ files/nvim/lua/plugins.lua
@@ -1,22 +1,29 @@
-require("rocks-setup")
+local rocks = require("rocks-setup")
 
-require("plugin-configs.vim-airline")
-require("plugin-configs.zenburn")
-require("plugin-configs.coq_nvim")
-require("plugin-configs.nvim-lspconfig")
-
-local function neorg_config()
-    require('neorg').setup {
-        load = {
-            ["core.defaults"] = {},
-            ["core.concealer"] = {},
-            ["core.dirman"] = {
-                config = {
-                    workspaces = {
-                        notes = "~/notes",
-                    }
-                },
-            },
-        },
-    }
+if not rocks then
+    vim.notify("Rocks is not installed. Install it with luarocks to enable plugins.")
+    return
 end
+
+local require = require("safe-require")
+
+-- require("plugin-configs.vim-airline")
+-- require("plugin-configs.zenburn")
+-- require("plugin-configs.coq_nvim")
+-- require("plugin-configs.nvim-lspconfig")
+--
+-- local function neorg_config()
+--     require('neorg').setup {
+--         load = {
+--             ["core.defaults"] = {},
+--             ["core.concealer"] = {},
+--             ["core.dirman"] = {
+--                 config = {
+--                     workspaces = {
+--                         notes = "~/notes",
+--                     }
+--                 },
+--             },
+--         },
+--     }
+-- end
